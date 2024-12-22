c30b5c156676d6d0140e19fdb8dd2932a18ab40e
harsh rawat <42332761+tetrex@users.noreply.github.com>
2024-12-22 22:01:08 +0530

    fix: device id , added more parameters

--- utils/custom_middleware/device_id.go
+++ utils/custom_middleware/device_id.go
@@ -13,9 +13,23 @@ func middleware(next echo.HandlerFunc) echo.HandlerFunc {
 		if userAgent == "" {
 			userAgent = "unknown"
 		}
+		ua_branding := c.Request().Header.Get("sec-ch-ua")
+		if userAgent == "" {
+			userAgent = "unknown-useragent-branding"
+		}
+		ua_mobile := c.Request().Header.Get("sec-ch-ua-mobile")
+		if userAgent == "" {
+			userAgent = "unknown-useragent-mobile"
+		}
+
+		ua_platform := c.Request().Header.Get("sec-ch-ua-platform")
+		if userAgent == "" {
+			userAgent = "unknown-useragent-mobile"
+		}
+
 		remoteIP := c.RealIP()
 
-		data := userAgent + remoteIP
+		data := userAgent + remoteIP + ua_branding + ua_mobile + ua_platform
 
 		hash := sha256.Sum256([]byte(data))
 		deviceID := hex.EncodeToString(hash[:])
